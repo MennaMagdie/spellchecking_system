d38c77e53ebc7c613a79cdeb515c51bf2c900757
Menna Magdy <mennamagdyx@gmail.com>
2023-05-22 22:36:11 +0300

    Updated.

--- main.c
+++ main.c
@@ -44,10 +44,36 @@ void display_inorder(node* root)
     }
 }
 
+long height(node* root) {
+    if (root == NULL) {
+        return -1;
+    } else {
+        int left_height = height(root->left);
+        int right_height = height(root->right);
+        return 1+(left_height > right_height ? left_height : right_height);
+    }
+}
+
+node *search(node *root, char value[100])
+{
+    if(root)
+    {
+        if(strcasecmp(root->data,value) == 0)
+            return root;
+        else if(strcasecmp(root->data,value) > 0)
+            return search(root->left, value);
+        else
+            return search(root->right, value);
+    }
+    else
+        return NULL;
+}
+
+
 
 int main()
 {
-    int i=0;
+    int nodes_num=0;
     node *root=NULL;
     FILE *f;
     f = fopen("EN-US-Dictionary.txt","r");
@@ -58,17 +84,19 @@ int main()
     char word[100];
     fscanf(f,"%s",word);
     root = insert(root,word);
-    i++;
+    nodes_num++;
     //printf("%s  ",word);
         }
-        printf("\n");
+        printf("Dictionary loaded successfully!\n");
     fclose(f);
     }
     else
         printf("FILE NOT FOUND.");
-        printf("number of nodes = %d\n",i);
-        //printf("number of levels = %.0f\n",ceil(log2(i))+1);//ceil(log2(n+1))
-    display_inorder(root);
+        printf("number of nodes = %d\n",nodes_num);
+        printf("number of levels = %d\n",height(root));
+    //display_inorder(root);
+
+    printf("\nword = %s",search(root,"torpedo")->data);
 
 
     return 0;
